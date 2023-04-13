45578c26d2e6d44b81ce317276ed02fea71dfade
= <=>
2023-04-13 11:49:42 -0300

    New: Adding a template route system.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import Routing from "./routes/Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={Routing} />
+  </React.StrictMode>
 );
---
+++ src/routes/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const Routing = createBrowserRouter([
+  {
+    path: "/",
+    element: "",
+  },
+  {
+    path: "*",
+    element: "",
+  },
+]);
